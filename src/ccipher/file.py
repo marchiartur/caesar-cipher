3b121b996845c29ef5c20b2a7215adea66655309
Artur Pacagnan <marchiartur@gmail.com>
2023-03-23 19:42:17 -0300

    adding package files

--- src/ccipher/file.py
+++ src/ccipher/file.py
@@ -1,14 +1,16 @@
-def readFile(filename):    
+def readFile(filename: str) -> str:    
     with open(filename, 'r', encoding='utf-8') as f:
         fileContent = f.read()
     
     return fileContent
 
-def writeFile(filename, string):
+def writeFile(filename: str, string: str) -> bool:
     try:
         with open(filename, 'w', encoding='utf-8') as f:
             f.write(string)
         
         return True
     except:
-        raise Exception('error to write in file')
+        Exception('error to write in file')
+
+        return False
